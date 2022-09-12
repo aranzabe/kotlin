ba229fd10a0b1102cf68baa560f57d2c5599bd66
Fernando Aranzabe <faranzabe@gmail.com>
2022-09-12 17:33:48 +0200

    Inicial

--- ejemplo/src/main.kt
+++ ejemplo/src/main.kt
@@ -1,3 +1,51 @@
 fun main(){
-    println("Hola")
+    var a = 12
+
+    if (a > 0){
+        println("Mayor que 0")
+    }
+    else {
+        println("Menor")
+    }
+
+    for (i in 1..a)
+    {
+        print("$i ")
+    }
+    println()
+
+    var cont : Int = 1
+    while(cont <= 3){
+        println(cont)
+        cont++
+    }
+
+    print("Dame una palabra: ")
+    val palabra = readLine()
+    println("Palabra leída: ${palabra}")
+
+    var numFinal = 2
+    try {
+         numFinal = readLine()!!.toInt()
+    }
+    catch (e:Exception){
+        println("Error: '${e.message}'")
+    }
+
+    cont = 1
+    do{
+        println(cont)
+        cont++
+    }while(cont <= numFinal)
+
+    val opcion = 21
+    val resultado = when(opcion){
+        0,2 -> "Pares"
+        1,3 -> "Impares"
+        else -> "Indeterminado"
+    }
+
+    println(resultado)
+
+
 }
